96b526c5d29762c69b27ddc4c2650db5be0cb89e
yujaemin <woawoa0513@gmail.com>
2022-12-16 19:59:49 +0900

    refactor: 기능 선택 에러 메시지 세분화
    잘못된 길이로 입력받았을 때와, 길이는 맞지만 잘못된 값일 때를 구분

--- src/main/java/pairmatching/message/ErrorMessage.java
+++ src/main/java/pairmatching/message/ErrorMessage.java
@@ -5,7 +5,8 @@ import static pairmatching.util.Constant.MAX_FUNCTION_NUMBER;
 
 public class ErrorMessage {
     private static final String HEADER = "[ERROR] ";
-    public static final String INCORRECT_FUNCTION_SELECTION = HEADER + "존재하지 않는 선택지를 입력하였습니다.";
+    public static final String INCORRECT_FUNCTION_INPUT = HEADER + "잘못된 값을 입력하였습니다.";
+    public static final String INCORRECT_FUNCTION_SELECTION = HEADER + "선택지에 존재하지 않는 값을 입력하였습니다.";
     public static final String INCORRECT_FUNCTION_NUMBER = String.format("%s %d~%d번 외의 선택지는 존재하지 않습니다.",
             HEADER, MIN_FUNCTION_NUMBER, MAX_FUNCTION_NUMBER);
     public static final String INCORRECT_COURSE_INPUT = HEADER + "존재하지 않는 과정명을 입력하였습니다.";
