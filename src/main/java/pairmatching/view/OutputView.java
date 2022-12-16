1ca66abb2b4f5aa12d2b13a395d2ee1c04336e46
yujaemin <woawoa0513@gmail.com>
2022-12-16 20:19:47 +0900

    feat: 페어 초기화 안내문 출력 함수 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -16,4 +16,8 @@ public class OutputView {
         System.out.println(NoticeMessage.REMATCHING_COMMAND_INPUT_NOTICE);
     }
 
+    public void printPairInitializeNotice() {
+        System.out.println(NoticeMessage.PAIR_INITIALIZE_NOTICE);
+    }
+
 }
