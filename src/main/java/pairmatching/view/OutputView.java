9f44621c5d7d6379734896f57fb11185371cbb42
yujaemin <woawoa0513@gmail.com>
2022-12-16 20:42:50 +0900

    feat: Output View 클래스 생성

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,11 @@
+package pairmatching.view;
+
+import pairmatching.message.NoticeMessage;
+
+public class OutputView {
+
+    public void printMainMenu() {
+        System.out.println(NoticeMessage.FUNCTION_SELECTION_NOTICE + '\n');
+    }
+
+}
