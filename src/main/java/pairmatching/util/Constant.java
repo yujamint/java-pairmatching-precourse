5342d02889662ba483aaea7319660ed48e596f89
yujaemin <woawoa0513@gmail.com>
2022-12-16 20:34:19 +0900

    feat: 매칭 여부 상수 선언
    매칭 여부(네 / 아니오) 상수 선언

--- src/main/java/pairmatching/util/Constant.java
+++ src/main/java/pairmatching/util/Constant.java
@@ -3,4 +3,7 @@ package pairmatching.util;
 public class Constant {
     public static final int MIN_FUNCTION_NUMBER = 1;
     public static final int MAX_FUNCTION_NUMBER = 3;
+
+    public static final String YES_MATCHING = "네";
+    public static final String NO_MATCHING = "아니오";
 }
