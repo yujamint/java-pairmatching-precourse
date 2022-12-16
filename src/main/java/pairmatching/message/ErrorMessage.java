765bddd9ccf268d4250374d8df0a08b72ab7266f
yujaemin <woawoa0513@gmail.com>
2022-12-16 19:03:23 +0900

    feat: 상세 정보 관련 에러메시지 추가
    상세 정보를 입력할 때 발생할 수 있는 예외에 대한 에러메시지 추가

--- src/main/java/pairmatching/message/ErrorMessage.java
+++ src/main/java/pairmatching/message/ErrorMessage.java
@@ -8,5 +8,8 @@ public class ErrorMessage {
     public static final String INCORRECT_FUNCTION_SELECTION = HEADER + "존재하지 않는 선택지를 입력하였습니다.";
     public static final String INCORRECT_FUNCTION_NUMBER = String.format("%s %d~%d번 외의 선택지는 존재하지 않습니다.",
             HEADER, MIN_FUNCTION_NUMBER, MAX_FUNCTION_NUMBER);
+    public static final String INCORRECT_COURSE_INPUT = HEADER + "존재하지 않는 과정명을 입력하였습니다.";
+    public static final String INCORRECT_LEVEL_INPUT = HEADER + "해당 과정에 존재하지 않는 레벨을 입력하였습니다.";
+    public static final String INCORRECT_MISSION_INPUT = HEADER + "해당 레벨에 존재하지 않는 미션을 입력하였습니다.";
 
 }
