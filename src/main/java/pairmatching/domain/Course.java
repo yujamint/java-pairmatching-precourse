1e00ef837754af1932b3c19da06e5f310b05ae14
yujaemin <woawoa0513@gmail.com>
2022-12-16 18:07:56 +0900

    feat: 과정 이름 List 반환 함수 구현
    과정의 이름을 List에 담아 반환하는 함수 구현

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,5 +1,8 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
@@ -8,5 +11,15 @@ public enum Course {
 
     Course(String name) {
         this.name = name;
+    }
+
+    public static List<String> names() {
+        List<String> courseNames = new ArrayList<>();
+
+        for (Course course : values()) {
+            courseNames.add(course.name);
+        }
+
+        return courseNames;
     }
 }
