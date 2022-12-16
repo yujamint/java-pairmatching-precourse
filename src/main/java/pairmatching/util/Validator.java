5ae4a658e950ce27931ac8b74b6b8c33c5b2e5be
yujaemin <woawoa0513@gmail.com>
2022-12-16 16:59:30 +0900

    feat: 기능 선택 입력값 길이 검사 함수 구현

--- src/main/java/pairmatching/util/Validator.java
+++ src/main/java/pairmatching/util/Validator.java
@@ -1,8 +1,12 @@
 package pairmatching.util;
 
 public class Validator {
-
     private static final String QUIT = "Q";
+    private static final int LENGTH_OF_FUNCTION_SELECTION = 1;
+
+    private boolean isCorrectLength(String input) {
+        return input.length() == LENGTH_OF_FUNCTION_SELECTION;
+    }
 
     private boolean isDigit(String input) {
         return input.chars().allMatch(Character::isDigit);
