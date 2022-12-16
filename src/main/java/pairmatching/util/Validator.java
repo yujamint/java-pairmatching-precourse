1d3bb309b0609591c29a1a341ad87d0fd1f48791
yujaemin <woawoa0513@gmail.com>
2022-12-16 17:04:30 +0900

    feat: 메뉴 선택지 숫자 범위 검사 함수 구현

--- src/main/java/pairmatching/util/Validator.java
+++ src/main/java/pairmatching/util/Validator.java
@@ -2,14 +2,21 @@ package pairmatching.util;
 
 public class Validator {
     private static final String QUIT = "Q";
-    private static final int LENGTH_OF_FUNCTION_SELECTION = 1;
+    private static final int LENGTH_OF_FUNCTION_OPTION = 1;
+    private static final int MIN_OPTION = 1;
+    private static final int MAX_OPTION = 3;
 
     private boolean isCorrectLength(String input) {
-        return input.length() == LENGTH_OF_FUNCTION_SELECTION;
+        return input.length() == LENGTH_OF_FUNCTION_OPTION;
     }
 
     private boolean isDigit(String input) {
         return input.chars().allMatch(Character::isDigit);
+    }
+
+    private boolean isValidRange(String input) {
+        int functionOption = input.charAt(0) - '0';
+        return functionOption >= MIN_OPTION && functionOption <= MAX_OPTION;
     }
 
     private boolean isQuitCommand(String input) {
