8a432293a7f594a352524a70beab363c7ec30403
yujaemin <woawoa0513@gmail.com>
2022-12-16 19:06:11 +0900

    feat: 상세 정보 입력 유효성 검사 함수 구현
    과정, 레벨, 미션 별로 유효성 검사를 진행

--- src/main/java/pairmatching/util/Validator.java
+++ src/main/java/pairmatching/util/Validator.java
@@ -5,11 +5,15 @@ import static pairmatching.util.Constant.MIN_FUNCTION_NUMBER;
 
 import pairmatching.domain.Course;
 import pairmatching.domain.Level;
+import pairmatching.domain.Mission;
 import pairmatching.message.ErrorMessage;
 
 public class Validator {
     private static final String QUIT = "Q";
     private static final int LENGTH_OF_FUNCTION_SELECTION = 1;
+    private static final int COURSE_INDEX = 0;
+    private static final int LEVEL_INDEX = 1;
+    private static final int MISSION_INDEX = 2;
 
     public static void validateFunctionSelection(String input) {
         if (!isCorrectLength(input)) {
@@ -20,6 +24,23 @@ public class Validator {
         }
         if (!isDigit(input) && !isQuitCommand(input)) {
             throw new IllegalArgumentException(ErrorMessage.INCORRECT_FUNCTION_SELECTION);
+        }
+    }
+
+    public static void validateDetailInformation(String input) {
+        String[] splitInput = input.split(", ");
+        String inputCourse = splitInput[COURSE_INDEX];
+        String inputLevel = splitInput[LEVEL_INDEX];
+        String inputMission = splitInput[MISSION_INDEX];
+
+        if (!isValidCourseInput(inputCourse)) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_COURSE_INPUT);
+        }
+        if (!isValidLevelInput(inputLevel)) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_LEVEL_INPUT);
+        }
+        if (!isValidMissionInput(inputLevel, inputMission)) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_MISSION_INPUT);
         }
     }
 
